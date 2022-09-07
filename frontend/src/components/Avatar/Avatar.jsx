9ef75ba9aa77c0179d05b5f156725598b4eb8e09
Yohh <durandyohan@zaclys.net>
2022-09-07 20:48:55 +0200

    turn Avatar.jsx to a button

--- frontend/src/components/Avatar/Avatar.jsx
+++ frontend/src/components/Avatar/Avatar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useAvatarContext } from "../../context";
 
 import "./Avatar.scss";
 
-export const Avatar = ({ avatarStatus }) => {
+export const Avatar = () => {
+  const { avatarStatus, setAvatarStatus } = useAvatarContext();
+
   return (
-    // eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events
-    <div className={avatarStatus ? "avatar on" : "avatar off"} />
+    <button
+      type="button"
+      className={avatarStatus ? "avatar on" : "avatar off"}
+      onClick={() => setAvatarStatus(!avatarStatus)}
+    />
   );
 };
