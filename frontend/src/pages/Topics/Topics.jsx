4c49662b33c22e8b5be323f650440caa3e9a8453
Yohh <durandyohan@zaclys.net>
2022-09-12 16:53:08 +0200

    ok

--- frontend/src/pages/Topics/Topics.jsx
+++ frontend/src/pages/Topics/Topics.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar } from "@components/";
+import ReactMarkdown from "react-markdown";
 
 export const Topics = () => {
+  const [input, setInput] = useState("");
+
   return (
     <div className="Topics">
+      <div className="test">
+        <div className="topic">
+          <ReactMarkdown className="markdown">{input}</ReactMarkdown>
+        </div>
+        <input
+          type="text"
+          className="editor"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
       <Navbar />
     </div>
   );
