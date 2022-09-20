ae3e68d94a8c519039e7a81d678a4cd135c63b14
Yohh <durandyohan@zaclys.net>
2022-09-20 16:16:00 +0200

    cleaning backend names and folders

--- backend/src/controllers/usersControllers.js
+++ backend/src/controllers/usersControllers.js
@@ -49,21 +49,8 @@ const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
     });
 };
 
-const getCategories = (req, res) => {
-  neuron
-    .query(`select * from categories`)
-    .then(([categories]) => {
-      res.status(201).json(categories);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
-
 module.exports = {
   getUsers,
   createUser,
-  getCategories,
   getUserByEmailWithPasswordAndPassToNext,
 };
