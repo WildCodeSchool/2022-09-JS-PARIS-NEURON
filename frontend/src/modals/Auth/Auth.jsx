4c24f3d7a0a869cc5b6948e1031a21ec486ad168
Aline3485 <99104872+Aline3485@users.noreply.github.com>
2022-09-21 10:12:24 +0200

    modal mobile first ok, Login and Register ok in auth.jsx

--- frontend/src/modals/Auth/Auth.jsx
+++ frontend/src/modals/Auth/Auth.jsx
@@ -33,59 +33,65 @@ export const Auth = ({ show, hide }) => {
   return show ? (
     <div className="auth">
       <div className="auth_button">
-      <div className="auth_button_close">
-      <button type="button"  onClick={()=> hide()}>X</button>
-      </div>
-      <div className="auth_form">
-        <form
-          className="auth_form_register auth_form_single"
-          action=""
-          onSubmit={(e) => handleRegister(e)}
-        >
-          <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="text"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <label htmlFor="mail">Mail</label>
-          <input
-            id="mail"
-            type="text"
-            value={mail}
-            onChange={(e) => setMail(e.target.value)}
-          />
-          <button type="submit" className="register">
-            Register
+        <div className="auth_button_close">
+          <button type="button" onClick={() => hide()}>
+            X
           </button>
-        </form>
-        <form className="auth_form_login auth_form_single" action="" onSubmit={(e) => handleLogin(e)}>
-          <label htmlFor="loginMail">Mail</label>
-          <input
-            id="loginMail"
-            type="text"
-            value={loginMail}
-            onChange={(e) => setLoginMail(e.target.value)}
-          />
-          <label htmlFor="loginPassword">Password</label>
-          <input
-            id="loginPassword"
-            type="text"
-            value={loginPassword}
-            onChange={(e) => setLoginPassword(e.target.value)}
-          />
-          <button type="submit" className="login">
-            Login
-          </button>
-        </form>
+        </div>
+        <div className="auth_form">
+          <form
+            className="auth_form_register auth_form_single"
+            action=""
+            onSubmit={(e) => handleRegister(e)}
+          >
+            <label htmlFor="username">Username</label>
+            <input
+              id="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              type="text"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label htmlFor="mail">Mail</label>
+            <input
+              id="mail"
+              type="text"
+              value={mail}
+              onChange={(e) => setMail(e.target.value)}
+            />
+            <button type="submit" className="register">
+              Register
+            </button>
+          </form>
+          <form
+            className="auth_form_login auth_form_single"
+            action=""
+            onSubmit={(e) => handleLogin(e)}
+          >
+            <label htmlFor="loginMail">Mail</label>
+            <input
+              id="loginMail"
+              type="text"
+              value={loginMail}
+              onChange={(e) => setLoginMail(e.target.value)}
+            />
+            <label htmlFor="loginPassword">Password</label>
+            <input
+              id="loginPassword"
+              type="text"
+              value={loginPassword}
+              onChange={(e) => setLoginPassword(e.target.value)}
+            />
+            <button type="submit" className="login">
+              Login
+            </button>
+          </form>
         </div>
       </div>
     </div>
