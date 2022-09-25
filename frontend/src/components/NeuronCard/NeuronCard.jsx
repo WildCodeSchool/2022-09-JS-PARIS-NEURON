2866b6925d9048b3cb9e350efe3481710ce42043
dexter-cut <r.bassot.wcs@gmail.com>
2022-09-25 23:35:54 +0200

    add 2 png ( emptyHeart + fullRedHeart), handleclick isfavoriteon NeuronCard component, keyframes emptyHeart to redFullHeart

--- frontend/src/components/NeuronCard/NeuronCard.jsx
+++ frontend/src/components/NeuronCard/NeuronCard.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+// import React, { useState } from "react";
 import { Avatar } from "@components/";
 import "./NeuronCard.scss";
 
 export const NeuronCard = () => {
+  //   const [isFavorite, setIsFavorite] = useState(false);
+  //   function handleClickFavorite() {
+  //     setIsFavorite(!isFavorite);
+  //   }
+
   return (
     <div className="NeuronCard">
+      {/* // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
+      {/* <div className={isFavorite ? "isFavorite" : "notFavorite"}>
+        <img
+          src="frontend/src/assets/images/emptyHeart.png"
+          id="favorite"
+          onClick={handleClickFavorite}
+          alt="heart"
+        />
+      </div> */}
       <Avatar />
     </div>
   );
