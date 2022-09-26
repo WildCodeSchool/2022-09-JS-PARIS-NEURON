3fc72baef70fa0cb8750ffc8e3d3a8cd657a5a5f
dexter-cut <r.bassot.wcs@gmail.com>
2022-09-26 14:06:29 +0200

    add handleSearch logic in NeuronFavorites.jsx

--- frontend/src/components/NeuronFavorites/NeuronFavorites.jsx
+++ frontend/src/components/NeuronFavorites/NeuronFavorites.jsx
@@ -1,17 +1,23 @@
-import { NeuronCard } from "@components/index";
+import { NeuronCard, Search } from "@components/index";
 import Carousel from "react-grid-carousel";
 import React, { useEffect, useState } from "react";
 import "./NeuronFavorites.scss";
 
 export const NeuronFavorites = () => {
   const [neurons, setNeurons] = useState([]);
-  // const [searchUser, setSearchUser] = useState("");
+  const [searchUser, setSearchUser] = useState("");
+  // const [usersById, setUsersById] = useState([]);
+  // const handleSearch = () => {
+  //   getUsersById(setUsersById);
+  // };
 
   useEffect(() => {
     setNeurons([
       1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
     ]);
   }, []);
+
+  console.warn(searchUser);
 
   const neuronFavList = [
     {
@@ -38,34 +44,32 @@ export const NeuronFavorites = () => {
   ];
 
   return (
-    <>
-      <div className="users_filter">
-        {/* <Search
-          placeholder="rechercher un topic"
-          content={setSearchUser}
-          handleSearch={handleSearch}
-        /> */}
-      </div>
+    <div className="carousel">
+      <Carousel
+        cols={10}
+        rows={1}
+        gap={10}
+        responsiveLayout={neuronFavList}
+        mobileBreakpoint={0}
+        showDots
+      >
+        {neurons.map((neuron) => (
+          <Carousel.Item key={neuron}>
+            <div className="item">
+              <NeuronCard />
+              <span className="item_pseudo">pseudo</span>
+            </div>
+          </Carousel.Item>
+        ))}
+      </Carousel>
 
-      <div className="carousel">
-        <Carousel
-          cols={10}
-          rows={1}
-          gap={10}
-          responsiveLayout={neuronFavList}
-          mobileBreakpoint={0}
-          showDots
-        >
-          {neurons.map((neuron) => (
-            <Carousel.Item key={neuron}>
-              <div className="item">
-                <NeuronCard />
-                <span className="item_pseudo">pseudo</span>
-              </div>
-            </Carousel.Item>
-          ))}
-        </Carousel>
+      <div className="users_filter">
+        <Search
+          placeholder="rechercher un Neuron"
+          content={setSearchUser}
+          // handleSearch={handleSearch}
+        />
       </div>
-    </>
+    </div>
   );
 };
