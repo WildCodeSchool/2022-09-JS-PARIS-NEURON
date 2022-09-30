297ac6ccb5751d7c560984d08d653e6df46c6683
Aline3485 <99104872+Aline3485@users.noreply.github.com>
2022-09-30 10:14:07 +0200

    nothing just space

--- frontend/src/components/PrivateMessage/PrivateMessage.jsx
+++ frontend/src/components/PrivateMessage/PrivateMessage.jsx
@@ -6,20 +6,19 @@ import rehypeHighlight from "rehype-highlight";
 export const PrivateMessage = ({ username, subject, content }) => {
   return (
     <div className="privatemessage">
-      
-        <div className="privatemessage_header">
-          <div className="privatemessage_avatarsender">
-            {" "}
-            <Avatar />
-          </div>
+      <div className="privatemessage_header">
+        <div className="privatemessage_avatarsender">
+          {" "}
+          <Avatar />
+        </div>
 
-          <span className="privatemessage_pseudosender">{username}</span>
-          <span className="privatemessage_subject">{subject}</span>
-        </div>
-        <div className="privatemessage_content">
-          <p className="content">{content}</p>
-        </div>
-      
+        <span className="privatemessage_pseudosender">{username}</span>
+        <span className="privatemessage_subject">{subject}</span>
+      </div>
+      <div className="privatemessage_content">
+        <p className="content">{content}</p>
+      </div>
+
       <ReactMarkdown
         className="markdown"
         rehypePlugins={[[rehypeHighlight, { ignoreMissing: true }]]}
