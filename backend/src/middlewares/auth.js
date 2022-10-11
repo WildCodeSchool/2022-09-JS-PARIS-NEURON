fc68607209b4bb48bb096d4fc09255f355101217
Yohh <durandyohan@zaclys.net>
2022-10-11 11:50:40 +0200

    update cookies on secure

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -51,7 +51,7 @@ const verifyPassword = (req, res, next) => {
         delete req.user.hashedpassword;
         res.cookie("token", token, {
           httpOnly: true,
-          // secure: true,
+          secure: true,
           maxAge: 24 * 60 * 60 * 1000,
         });
         if (req.body.updatingSettings) {
